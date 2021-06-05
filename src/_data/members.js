d12cf9de4403d83ba7360bcc39c3303b185081de
Marcus Rådell <marcus@radell.net>
2021-06-06 00:23:59 +0200

    Add some members data.

---
+++ src/_data/members.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    slug: "marcusradell",
+    name: "Marcus Rådell",
+    birthdate: "1982-03-03",
+    experiences: [
+      {
+        company: "Vembla",
+        role: "Director of Engineering",
+      },
+    ],
+  },
+];
